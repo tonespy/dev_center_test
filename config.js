d5bd4dcd9fb5f4a9176a0b3214952efebde35026
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-02-17 11:25:31 +0100

    Config file for storing: * Twitter key configs * Prompt data

--- config.js
+++ config.js
@@ -5,9 +5,20 @@
  * access_token
  * access_token_secret
  */
-module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+const config = {
+    consumer_key: process.env.bot_twitter_consumer_key,
+    consumer_secret: process.env.bot_twitter_consumer_secret,
+    access_token: process.env.bot_twitter_access_token,
+    access_token_secret: process.env.bot_twitter_access_token_secret,
+    path: './out/'
 }
+
+const questions = [
+    {
+        type: 'input',
+        name: 'hashtag',
+        message: 'Enter hashtag(s) separated by comma or space (nba, #basketall football, javelin)'
+    }
+];
+
+module.exports = { questions, config }
